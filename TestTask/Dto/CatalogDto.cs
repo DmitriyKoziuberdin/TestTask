f29e9cd654b55d658a3bd1cd66b26abcf636efe6
Dmitriy Koziuberdin <mitakoz581@gmail.com>
2023-11-17 23:35:26 +0200

    Added a way to import and export data.

--- TestTask/Dto/CatalogDto.cs
+++ TestTask/Dto/CatalogDto.cs
@@ -1,9 +1,19 @@
-﻿namespace TestTask.Dto
+﻿using Newtonsoft.Json;
+
+namespace TestTask.Dto
 {
     public class CatalogDto
     {
+        [JsonProperty("CatalogId")]
         public int CatalogId { get; set; }
+
+        [JsonProperty("Name")]
         public string? Name { get; set; }
+
+        [JsonProperty("ParentCatalogId")]
+        public int? ParentCatalogId { get; set; }
+
+        [JsonProperty("SubCatalogs")]
         public List<CatalogDto>? SubCatalogs { get; set; }
     }
 }
